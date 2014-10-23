bc2fe443e3c4034db2ac01733521c6d52afde291
rasstislav <brandobur1@stud.uniza.sk>
2014-10-23 13:19:57 +0200

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,48 +1,127 @@
-function posunSlide(stary,novy,rychlost)
+var slider = document.getElementById("slider");
+slider.style.position = "relative";
+
+var slides = slider.getElementsByClassName("slide");
+var pocet = slides.length;
+
+function posunSlideDalsi(stary,aktualny,novy,rychlost)
 {
   $(stary).fadeOut(rychlost,"swing");
-  $(novy).fadeIn(rychlost,"swing");
+
+  $(stary).promise().done(function()
+  {
+    $(stary).removeAttr('id');
+    $(aktualny).attr('id','slide-prioritny');
+    $(novy).attr('id','slide-nasledujuci');
+
+    stary.style.display = "";
+
+    done=true;
+  });
+}
+
+function posunSlidePredosli(stary,aktualny,novy)
+{
+  $(stary).fadeOut(500,"swing");
+
+  $(stary).promise().done(function()
+  {
+    $(stary).attr('id','slide-nasledujuci');
+    $(aktualny).attr('id','slide-prioritny');
+
+    stary.style.display = "";
+
+    done=true;
+  });
 }
 
 var timer = function()
 {
   if (!$(slider).is(':hover'))
-    posunSlide(slides[index], slides[++index==pocet ? index=0 : index],700);
+    $('#nasledujuci').trigger('click');
 };
 
-var slider = document.getElementById("slider");
-slider.style.position = "relative";
-slider.style.overflow = "hidden";
-
-var slides = slider.getElementsByClassName("slide");
-var pocet = slides.length;
-
-document.getElementById("predchadzajuci").style.display = "block";
-document.getElementById("nasledujuci").style.display = "block";
-
 var index = 0;
+var aktind = 0;
 var znova = false;
-var i;
-for (i = 0; i < pocet; i++)
-  slides[i].style.position = "absolute";
-
-var cas = 5000;
+var cas = 2000;
 var interval;
+var done = true;
 
 var obrazok = slides[index].getElementsByTagName("img")[0];
 if($(slides[index].getElementsByClassName("slide-obrazok")[0]).css("float")!="left")
   interval = setInterval(timer,cas);
 
-$(document).on('click', '#predchadzajuci', function () {
-  posunSlide(slides[index], slides[--index==-1 ? index=pocet-1 : index],700);
-  clearInterval(interval);
-  interval = setInterval(timer,cas);
+$('#nasledujuci').click(function()
+{
+  if(done==true)
+  {
+    done = false;
+
+    index++;
+    if(index==pocet)
+      index = 0;
+
+    aktind = index;
+
+    var aktualny = slides[aktind];
+
+    var novy;
+    if(aktind+1==pocet)
+      novy = slides[0];
+    else
+      novy = slides[aktind+1];
+
+    var stary;
+    if(aktind-1==-1)
+      stary = slides[pocet-1];
+    else
+      stary = slides[aktind-1];
+
+    posunSlideDalsi(stary, aktualny, novy, 500);
+    clearInterval(interval);
+
+    if($(slides[index].getElementsByClassName("slide-obrazok")[0]).css("float")!="left")
+      interval = setInterval(timer,cas);
+  }
 });
 
-$(document).on('click', '#nasledujuci', function () {
-  posunSlide(slides[index], slides[++index==pocet ? index=0 : index],700);
-  clearInterval(interval);
-  interval = setInterval(timer,cas);
+$('#predchadzajuci').on('click', function()
+{
+  if(done==true)
+  {
+    done = false;
+
+    index--;
+    if(index==-1)
+      index = pocet-1;
+
+    aktind = index;
+
+    var aktualny = slides[aktind];
+
+    $(aktualny).attr('id','slide-nasledujuci');
+
+    var novy;
+    if(aktind-1==-1)
+      novy = slides[pocet-1];
+    else
+      novy = slides[aktind-1];
+
+    $(novy).removeAttr('id');
+
+    var stary;
+    if(aktind+1==pocet)
+      stary = slides[0];
+    else
+      stary = slides[aktind+1];
+
+    posunSlidePredosli(stary, aktualny, novy);
+    clearInterval(interval);
+
+    if($(slides[index].getElementsByClassName("slide-obrazok")[0]).css("float")!="left")
+      interval = setInterval(timer,cas);
+  }
 });
 
 $(window).resize(function()
@@ -51,7 +130,7 @@ $(window).resize(function()
 
   if($(slides[index].getElementsByClassName("slide-obrazok")[0]).css("float")=="left")
   {
-    posunSlide(slides[index], slides[index=0],0);
+    posunSlideDalsi(slides[index+1==pocet ? index : index+1], slides[index=0],slides[1], 0);
     clearInterval(interval);
     znova = true;
   }
@@ -64,3 +143,6 @@ $(window).resize(function()
     }
   }
 });
+
+document.getElementById("predchadzajuci").style.display = "block";
+document.getElementById("nasledujuci").style.display = "block";
